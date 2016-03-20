6c9c88da1962e8bb6ac74da843d996fb088a6f5f
linqing-lu <lulinqing@foxmail.com>
2016-03-20 21:13:03 +0800

    新建工程

---
+++ source/SwiftEloquent/SwiftEloquent/SwiftEloquent.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEloquent.h
+//  SwiftEloquent
+//
+//  Created by 卢林清 on 16/3/20.
+//  Copyright © 2016年 卢林清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEloquent.
+FOUNDATION_EXPORT double SwiftEloquentVersionNumber;
+
+//! Project version string for SwiftEloquent.
+FOUNDATION_EXPORT const unsigned char SwiftEloquentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEloquent/PublicHeader.h>
+
+
---
+++ source/SwiftEloquent/SwiftEloquent/SwiftEloquentProtocol.h
@@ -0,0 +1,13 @@
+//
+//  SwiftEloquentProtocol.h
+//  SwiftEloquent
+//
+//  Created by 卢林清 on 16/3/20.
+//  Copyright © 2016年 卢林清. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SwiftEloquentProtocol <NSObject>
+
+@end
